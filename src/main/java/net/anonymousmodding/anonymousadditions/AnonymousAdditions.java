4b8cfd37ff9e6931145e3a36e172e5bc3df3503f
AnonymousModding <marecaux.felix@gmail.com>
2024-11-24 17:05:27 -0800

    Added enchantment shards + glow effect.

--- src/main/java/net/anonymousmodding/anonymousadditions/AnonymousAdditions.java
+++ src/main/java/net/anonymousmodding/anonymousadditions/AnonymousAdditions.java
@@ -1,6 +1,8 @@
 package net.anonymousmodding.anonymousadditions;
 
 import com.mojang.logging.LogUtils;
+import net.anonymousmodding.anonymousadditions.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -34,6 +36,8 @@ public class AnonymousAdditions
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -46,7 +50,9 @@ public class AnonymousAdditions
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ENCHANTED_SHARDS);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/anonymousmodding/anonymousadditions/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.anonymousmodding.anonymousadditions.item;
+
+import net.anonymousmodding.anonymousadditions.AnonymousAdditions;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, AnonymousAdditions.MOD_ID);
+
+    public static final RegistryObject<Item> ENCHANTED_SHARDS = ITEMS.register("enchanted_shards", () -> new Item(new Item.Properties().component(DataComponents.ENCHANTMENT_GLINT_OVERRIDE, true)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
